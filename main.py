342fcf3ff45db2cce64a26e9f38141d67e9e8662
hsetyiupaing <hsetyiupaing367@gmail.com>
2024-12-31 23:44:42 +0700

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import  os
+from dotenv import load_dotenv, dotenv_values
 from typing import Final
 from telegram import  Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN: Final = '7238792622:AAFCHJUF6GH3zTF8_wmGND6zs97fg9G8cQM'
-BOT_USERNAME: Final = '@orange_123_bot'
+load_dotenv()
+
+TOKEN: Final = os.getenv('BOT_TOKEN')
+BOT_USERNAME: Final = os.getenv('BOT_USERNAME')
 
 #Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -18,16 +22,16 @@ async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 #Handle Responses
 def handle_responses(text: str) -> str:
     processed: str = text.lower()
-    if 'hello' or 'hi' in processed:
+    if 'hello' == processed or 'hi' == processed:
         return 'Hello There! Nice to meet you!'
 
-    elif 'how are you' in processed:
+    elif 'how are you' == processed:
         return 'I\'m fine, thank you!'
 
-    elif 'i love you' in processed:
+    elif 'i love you' == processed:
         return 'Thank You, That\'s wonderful'
 
-    elif 'i love python' in processed:
+    elif 'i love python' == processed:
         return 'I love python too!'
 
     return 'Sorry, I don\'t understand.'
@@ -37,18 +41,23 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     text = update.message.text
 
     print(f'User({update.message.from_user.id}) Type: {message_type} Text: {text}')
+
     if message_type == 'group':
+        # Check if the bot's username is mentioned
         if BOT_USERNAME in text:
             # Extract the remaining text after removing the username
             new_text = text.replace(BOT_USERNAME, '').strip()
             response = handle_responses(new_text)
         else:
+            # Ignore messages that don't mention the bot
             return
     else:
+        # Handle private messages
         response = handle_responses(text)
 
     print(f'Bot: {response}')
     await update.message.reply_text(response)
+
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print(f'Update {update} caused error {context.error}')
